584465bc65dad61d3765dea738117623460d30a9
Ayoub <ayoubjarhni@gmail.com>
2025-03-12 15:32:24 +0000

    initialiser project

---
+++ src/main/java/net/codejava/Main.java
@@ -0,0 +1,10 @@
+package net.codejava;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i=0 ; i<7 ; i++){
+            System.out.println(i);
+        }
+        System.out.println("Hello world!");
+    }
+}
