fd640bb59d3a86df8f99ca65a9dcba1fac3492ef
Ayoub <ayoubjarhni@gmail.com>
2025-03-14 00:13:44 +0000

    manipulate database

--- src/main/java/net/codejava/Main.java
+++ src/main/java/net/codejava/Main.java
@@ -3,7 +3,15 @@ package net.codejava;
 public class Main {
     public static void main(String[] args) {
       ManipulationMongo manipulationMongo = new ManipulationMongo();
-      manipulationMongo.createDb("Cars");
+
+
+
+      // manipulationMongo.createDb("Cars");
+        // manipulationMongo.deleteDB("Cars");
+        manipulationMongo.findAllCollections("Cars");
+       // manipulationMongo.createCollection("Cars","car");
+       // manipulationMongo.deleteCollection("Cars","car");
+        manipulationMongo.showCollection("Cars","car");
     }
 
 }
--- src/main/java/net/codejava/ManipulationMongo.java
+++ src/main/java/net/codejava/ManipulationMongo.java
@@ -6,6 +6,7 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ManipulationMongo {
     MongoDBConnection connection=new MongoDBConnection();
@@ -18,34 +19,122 @@ public class ManipulationMongo {
         }
     }
 
-public void createDb(String dbName){
-    MongoDatabase database =mongoClient.getDatabase(dbName);
-    MongoCollection<Document> collection = database.getCollection("sampleCollection");
-    System.out.println(dbName + " created successfully");
-}
+    private boolean isDbExist(String dbName) {
+        Iterable<String> liste = mongoClient.listDatabaseNames();
+        for (String db : liste) {
+            if (db.equals(dbName)) {
+                return true;
+            }
 
-    public void deleteDb(){
+        }
+        return false;
+    }
+
+
+    public void createDatabase(String dbName){
+    MongoDatabase db=mongoClient.getDatabase(dbName);
+    if (!isDbExist(dbName)){
+        db.createCollection("collection1");
+        db.getCollection("collection1").insertOne(new Document());
+        System.out.println(dbName +
+                " created succefully");
+    }
+        System.out.println("Database already exists");
+    }
+
+    public void deleteDB(String dbName){
+        if (isDbExist(dbName)){
+            MongoDatabase db=mongoClient.getDatabase(dbName);
+            for (String collection : db.listCollectionNames() ) {
+                db.getCollection(collection).drop();
+            }
+            System.out.println(dbName+ " deleted succefully");
+        }
+        else {
+            System.out.println("Database " + dbName + " does not exist");
+        }
 
     }
-public void findAllCollections(String db ){
 
-}
-public void createCollections(){
+    public void findAllCollections(String dbName ){
+      MongoDatabase db=mongoClient.getDatabase(dbName);
+      if (!isDbExist(dbName)){
+          System.out.println(dbName + " does not exist");
+      }
+      for (String collection : db.listCollectionNames() ) {
+          System.out.println(collection);
+      }
+    }
 
-}
-public void deleteCollections(){
+    public boolean isCollectionExist(String database, String collectionName) {
+        MongoDatabase db = mongoClient.getDatabase(database);
+        for (String name : db.listCollectionNames()) {
+            if (name.equals(collectionName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-}
-public void showCollections(){}
-public void addDocument(){
+    public void createCollection(String database, String collectionName){
+        MongoDatabase db=mongoClient.getDatabase(database);
+        if (!isDbExist(database)){
+            System.out.println(database + " does not exist");
+        }
+        if (!isCollectionExist(database,collectionName)){
+            db.createCollection(collectionName);
+            System.out.println( "Collection " + collectionName + " created succefully");
+        }
+        else{
+            System.out.println("Collection " + collectionName + " already exists");
+        }
+    }
 
-}
-public void deleteDocument(){
+    public void deleteCollection(String database, String collectionName){
+        MongoDatabase db=mongoClient.getDatabase(database);
+        if (!isDbExist(database)){
+            System.out.println(database + " does not exist");
+        }
 
-}
-public void updateDocument(){
+        if (isCollectionExist(database,collectionName)){
+            db.getCollection(collectionName).drop();
+            System.out.println( "Collection " + collectionName + " deleted succefully");
+        }
+        else{
+            System.out.println("Collection " + collectionName + " does not exist");
+        }
+    }
 
-}
+    public void showCollection(String dbName , String collection) {
+        MongoDatabase db=mongoClient.getDatabase(dbName);
+        if(!isDbExist(dbName)){
+            System.out.println(dbName + " does not exist");
+            return;
+        }
+
+        if (!isCollectionExist(dbName,collection)){
+        System.out.println(dbName + " does not exist");
+            return;
+        }
+        findDocuments(dbName,collection);
+    }
+
+    public void findDocuments(String dbName , String collection) {
+        MongoDatabase db=mongoClient.getDatabase(dbName);
+       if (!isDbExist(dbName)){
+           System.out.println(dbName + " does not exist");
+           return;
+       }
+       if (!isCollectionExist(dbName,collection)){
+           System.out.println(collection + " Collection  does not exist");
+           return;
+       }
+       MongoCollection<Document> mongoCollection=db.getCollection(collection);
+       Iterable<Document> documents=mongoCollection.find();
+       for (Document document : documents) {
+           System.out.println(document.toJson());
+       }
+    }
 
 
-}
+    }
