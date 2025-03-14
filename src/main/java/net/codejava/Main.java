0545244aff76602bc9690c9f91d72d78cfb9cdae
Ayoub <ayoubjarhni@gmail.com>
2025-03-14 14:16:31 +0000

    collection manipulation

--- src/main/java/net/codejava/Main.java
+++ src/main/java/net/codejava/Main.java
@@ -1,18 +1,6 @@
 package net.codejava;
 
 public class Main {
-    public static void main(String[] args) {
-      ManipulationMongo manipulationMongo = new ManipulationMongo();
 
-
-
-      // manipulationMongo.createDb("Cars");
-        // manipulationMongo.deleteDB("Cars");
-        manipulationMongo.findAllCollections("Cars");
-        manipulationMongo.showCollection("Cars","collection1");
-       // manipulationMongo.createCollection("Cars","car");
-       // manipulationMongo.deleteCollection("Cars","car");
-
-    }
 
 }
